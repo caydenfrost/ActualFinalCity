92f37993679eb14b7f7e11526bbc0ebf2cd6deec
caydenfrost <144173278+caydenfrost@users.noreply.github.com>
2024-02-17 10:09:37 -0800

    Tested git connection

---
+++ Assets/Scenes/Scripts/Pathfind.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class Pathfind : MonoBehaviour
+{
+    public NavMeshAgent agent;
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
