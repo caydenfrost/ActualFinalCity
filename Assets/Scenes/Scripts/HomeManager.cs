f2ec643fb00621c3ceffa67ab7ce7592f885f05c
caydenfrost <144173278+caydenfrost@users.noreply.github.com>
2024-02-26 13:46:21 -0800

    did a little, characters enter house now

--- Assets/Scenes/Scripts/HomeManager.cs
+++ Assets/Scenes/Scripts/HomeManager.cs
@@ -1,36 +1,84 @@
-using System.Collections;
 using System.Collections.Generic;
-using TMPro;
 using UnityEngine;
-using UnityEngine.UI;
 
 public class HomeManager : MonoBehaviour
 {
-    private Dictionary<GameObject, TMP_Text> characterStorage;
-    public GameObject currentCharacter;
+    public int maxResidentsPerHouse = 5; // Maximum allowed characters per house
+    private Dictionary<GameObject, List<CharacterStats>> characterHouses; // Dictionary to store character stats per house
+
     void Start()
     {
-        
+        characterHouses = new Dictionary<GameObject, List<CharacterStats>>();
     }
 
-    // Update is called once per frame
-    public void UpdateHousedCharacters(GameObject player, TMP_Text name)
+    public void UpdateDict(int characterID, GameObject house)
     {
-        if (characterStorage.ContainsKey(player))
+        if (characterID > 0 && house != null)
         {
-            characterStorage[player] = name;
+            // Check if house already has characters
+            if (!characterHouses.ContainsKey(house))
+            {
+                characterHouses.Add(house, new List<CharacterStats>());
+            }
+
+            // Check if house is full
+            if (characterHouses[house].Count < maxResidentsPerHouse)
+            {
+                // Add character stats to house dictionary
+                characterHouses[house].Add(new CharacterStats(characterID));
+            }
+            else
+            {
+                Debug.LogWarning("House is full!");
+            }
         }
         else
         {
-            characterStorage.Add(player, name);
+            Debug.LogWarning("Invalid character or house data.");
         }
     }
-    public void RemoveCharacter()
+
+    public void RemoveCharacter(GameObject character, GameObject house)
     {
-        currentCharacter.SetActive(true);
-        if (characterStorage.ContainsKey(currentCharacter.gameObject))
+        if (characterHouses.ContainsKey(house))
         {
-            characterStorage.Remove(currentCharacter.gameObject);
+            // Find character in house list and remove
+            for (int i = 0; i < characterHouses[house].Count; i++)
+            {
+                if (characterHouses[house][i].characterID == character.GetComponent<CharacterMove>().id)
+                {
+                    characterHouses[house].RemoveAt(i);
+                    break;
+                }
+            }
         }
+        else
+        {
+            Debug.LogWarning("House not found in dictionary.");
+        }
+    }
+
+    public List<CharacterStats> GetCharacterStats(GameObject house)
+    {
+        if (characterHouses.ContainsKey(house))
+        {
+            return characterHouses[house];
+        }
+        else
+        {
+            return null;
+        }
+    }
+}
+
+public class CharacterStats
+{
+    public int characterID;
+    // Add other character statistics here (e.g., name, health, job, etc.)
+
+    public CharacterStats(int id)
+    {
+        characterID = id;
+        // Initialize other character stats here
     }
 }
