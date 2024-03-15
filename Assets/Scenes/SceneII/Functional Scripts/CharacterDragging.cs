67c211a4b2dad726e338b2ad3a06a9ff418f4540
caydenfrost <144173278+caydenfrost@users.noreply.github.com>
2024-03-15 13:47:06 -0700

    did some bug fixing

--- Assets/Scenes/SceneII/Functional Scripts/CharacterDragging.cs
+++ Assets/Scenes/SceneII/Functional Scripts/CharacterDragging.cs
@@ -1,15 +1,20 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.AI;
 
 public class CharacterDragging : MonoBehaviour
 {
     private Vector3 initialMousePosition;
     private Vector3 initialCharacterPosition;
     private bool isDragging = false;
+    private NavMeshAgent navMeshAgent;
+    private Rigidbody rb;
+
     void Start()
     {
-        
+        navMeshAgent = GetComponent<NavMeshAgent>();
+        rb = GetComponent<Rigidbody>();
     }
 
     // Update is called once per frame
@@ -44,6 +49,22 @@ public class CharacterDragging : MonoBehaviour
                 transform.position = targetPosition;
             }
         }
+        else if (!isDragging)
+        {
+            // If not dragging, teleport to the closest position on NavMesh
+            if (navMeshAgent != null && navMeshAgent.isOnNavMesh)
+            {
+                NavMeshHit hit;
+                if (NavMesh.SamplePosition(transform.position, out hit, 10f, NavMesh.AllAreas))
+                {
+                    transform.position = hit.position;
+                    // Zero out the Rigidbody's velocity
+                    if (rb != null)
+                        rb.velocity = Vector3.zero;
+                }
+            }
+        }
+
         if (Input.GetMouseButtonUp(0))
         {
             isDragging = false;
